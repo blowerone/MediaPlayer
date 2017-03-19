7a1acad3a595ba86181738996e978992ef53c93f
Jack <737477675@qq.com>
2017-03-19 12:54:43 +0800

    Commit ListVew

--- app/src/main/java/com/example/wjk/mediaplay/VideoPagerAdapter/VideoPagerAdapter.java
+++ app/src/main/java/com/example/wjk/mediaplay/VideoPagerAdapter/VideoPagerAdapter.java
@@ -1,10 +1,14 @@
 package com.example.wjk.mediaplay.VideoPagerAdapter;
 
 import android.content.Context;
+import android.text.format.Formatter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.wjk.mediaplay.R;
 import com.example.wjk.mediaplay.domain.MediaItem;
 import com.example.wjk.mediaplay.utils.Utils;
 
@@ -43,10 +47,37 @@ public class VideoPagerAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewholder;
         //设置适配器的Item布局
-        if(convertView == null){
-
+        if(convertView == null) {
+            convertView = View.inflate(context, R.layout.item_video_pager, null);
+            //实例化viewholder
+            viewholder = new ViewHolder();
+            viewholder.iv_icon = (ImageView) convertView.findViewById(R.id.iv_icon);
+            viewholder.tv_name = (TextView) convertView.findViewById(R.id.tv_name);
+            viewholder.tv_time = (TextView) convertView.findViewById(R.id.tv_time);
+            viewholder.tv_size = (TextView) convertView.findViewById(R.id.tv_size);
+            convertView.setTag(viewholder);
+        }else{
+            viewholder = (ViewHolder) convertView.getTag();
         }
+
+        //根据position得到列表中对应位置的数据
+        MediaItem mediaItem = mediaItems.get(position);
+        viewholder.tv_name.setText(mediaItem.getName());
+        viewholder.tv_time.setText(utils.stringForTime((int) mediaItem.getDuration()));
+        viewholder.tv_size.setText(Formatter.formatFileSize(context,mediaItem.getSize()));
+
         return convertView;
+    }
+
+    /**
+     * 使用静态内部类可以在一个外部类对象中实例化多个静态内部类对象
+     */
+    static class ViewHolder{
+        ImageView iv_icon;
+        TextView tv_name;
+        TextView tv_time;
+        TextView tv_size;
     }
 }
