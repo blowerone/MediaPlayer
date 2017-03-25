e974c645195551de4d94ae40cb37eebb887b967f
Jack <737477675@qq.com>
2017-03-25 22:02:55 +0800

    Mediaplayer_top title

--- app/src/main/java/com/example/wjk/mediaplay/activity/SystemVideoPlayer.java
+++ app/src/main/java/com/example/wjk/mediaplay/activity/SystemVideoPlayer.java
@@ -1,6 +1,7 @@
 package com.example.wjk.mediaplay.activity;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
@@ -28,7 +29,37 @@ public class SystemVideoPlayer extends Activity {
             videoview.setVideoURI(uri);
         }
         //设置控制面板
-        videoview.setMediaController(new MediaController(this));
+//        videoview.setMediaController(new MediaController(this));
 
+        videoview.setOnPreparedListener(new MyOnPreparedListener());
+
+        videoview.setOnErrorListener(new MyOnErrorListener());
+
+        videoview.setOnCompletionListener(new MyOnCompletionListener());
+
+    }
+
+    class MyOnPreparedListener implements MediaPlayer.OnPreparedListener{
+
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            //当底层解码准备好的时候
+        }
+    }
+
+    class MyOnErrorListener implements MediaPlayer.OnErrorListener{
+
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+            return false;
+        }
+    }
+
+    class MyOnCompletionListener implements MediaPlayer.OnCompletionListener{
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+
+        }
     }
 }
