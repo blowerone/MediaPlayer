386c9f53f000c7e68c2a928cfc537474dee62cff
Jack <737477675@qq.com>
2017-04-08 23:51:20 +0800

    播放器被本地视频和网络视频调起

---
+++ StartAllPlayer/src/main/java/com/example/startallplayer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.startallplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startAllPlayer(View view){
+        Intent intent = new Intent();
+        intent.setDataAndType(Uri.parse("http://vfx.mtime.cn/Video/2016/07/19/mp4/160719095812990469.mp4"),"video/*");
+        startActivity(intent);
+    }
+}
